a7db953b9b0d78052ed76877a4822a4f70f83015
Viswesh Periyasamy <visweshrp@gmail.com>
2019-04-25 17:52:28 -0500

    updated smooth masks to use color dist

--- dataset_tools/smooth_masks.py
+++ dataset_tools/smooth_masks.py
@@ -5,6 +5,7 @@ import numpy as np
 from os import walk
 from scipy.misc import imsave
 from skimage.io import imread
+from scipy.spatial import distance
 
 # define detectable classes and corresponding colors
 # tuple keys are (R, G, B)
@@ -21,6 +22,7 @@ RGB_classes = {(128, 64, 128): 'paved-area',
 			   (2, 135, 115): 'obstacle',
 			   (0, 102, 0): 'grass' }
 
+RGB_class_mtx = np.array([*RGB_classes.keys()])
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -41,7 +43,7 @@ args = parser.parse_args()
 mask_dir = args.mask_dir
 out_dir = args.out_dir
 
-TOL = 10 # tolerance for matching mask
+TOL = 25 # tolerance for matching mask
 
 files = []
 for (dirpath, dirnames, filenames) in walk(mask_dir):
@@ -51,12 +53,10 @@ if not files:
     print('no images found in input directory')
     exit()
 
-for f in files:
+for f in files:#[::-1]:
     print('smoothing %s...' % f)
-    img_id = f[:-4]  # strip '.png'
-    img_path = '%s/%s' % (mask_dir, f)
 
-    # read in mask and real images
+    img_path = '%s/%s' % (mask_dir, f)
     img = imread(img_path, img_num=0)
 
     img_w = img.shape[1]  # number of columns is 'width'
@@ -65,20 +65,14 @@ for f in files:
     for i in range(img_h):
         for j in range(img_w):
 
-            # iterate over class colors
-            match = False
-            for RGB_key in RGB_classes.keys():
-                if  (img[i, j, 0] >= RGB_key[0] - TOL) \
-                and (img[i, j, 0] <= RGB_key[0] + TOL) \
-                and (img[i, j, 1] >= RGB_key[1] - TOL) \
-                and (img[i, j, 1] <= RGB_key[1] + TOL) \
-                and (img[i, j, 2] >= RGB_key[2] - TOL) \
-                and (img[i, j, 2] <= RGB_key[2] + TOL):
-                    match = True
-                    break
+            # compute euclidean distance to all class colors
+            px_color = [img[i, j, :]]
+            dist = distance.cdist(RGB_class_mtx, px_color, 'euclidean')
+            key_idx = np.argmin(dist) # choose closest one
 
-            if match:
-                img[i, j, :] = RGB_key
+            # if this color is too far from any of our classes, leave it be
+            if dist[key_idx] <= TOL:
+                img[i, j, :] = RGB_class_mtx[key_idx]
 
     # save box annotations of real img to out directory
     imsave('%s/%s' % (out_dir, f), img)

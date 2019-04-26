ff6aaab9259cb092c439f4f098f948c692f712fb
Viswesh Periyasamy <visweshrp@gmail.com>
2019-04-26 10:49:24 -0500

    updated split tools

--- dataset_tools/split_data.py
+++ dataset_tools/split_data.py
@@ -1,13 +1,12 @@
 from pathlib import Path
 import os
+import sys
 import shutil
 
-TRAIN_FILE = "unreal_splits/4000/train.split"
-TEST_FILE = "unreal_splits/4000/test.split"
-IMG_DIR = Path(
-    "/mnt/c/Users/viswe/Desktop/computer-science/CS 766/unreal_images_4000/images"
-)
-EXTENSION = "-outputs.png"
+TRAIN_FILE = sys.argv[1]
+TEST_FILE = sys.argv[2]
+IMG_DIR = Path(sys.argv[3])
+EXTENSION = ".jpg"
 
 with open(TRAIN_FILE) as f:
     train_imgs = [x.strip() + EXTENSION for x in f.readlines()]
@@ -17,14 +16,14 @@ with open(TEST_FILE) as f:
 
 print(os.listdir(IMG_DIR))
 
-train_path = Path(IMG_DIR, "train")
+train_path = IMG_DIR#Path(IMG_DIR, "train")
 if not os.path.exists(train_path):
     os.mkdir(train_path)
 for t in train_imgs:
     print(t)
     shutil.move(Path(IMG_DIR, t), Path(train_path, t))
 
-test_path = Path(IMG_DIR, "val")
+test_path = Path(IMG_DIR, "../test_boxes")
 if not os.path.exists(test_path):
     os.mkdir(test_path)
 
